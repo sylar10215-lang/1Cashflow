86d7bc7b71320755e3a1221c6c65aa0465c411b1
sylar10215-lang <sylar10215@gmail.com>
2026-08-18 06:33:32 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-const CACHE_NAME = 'mj-bank-v2';
+const CACHE_NAME = 'mj-cashflow-v1';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json'
 ];
 
+// Install Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// Fetching content
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
